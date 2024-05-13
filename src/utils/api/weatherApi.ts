dcfb8f2aa44e23f851749c384139f90cf396a81d
ChiaJunWei <83580418+ChiaJunWei@users.noreply.github.com>
2024-05-13 23:34:09 +0800

    api temp fix

--- src/utils/api/weatherApi.ts
+++ src/utils/api/weatherApi.ts
@@ -3,19 +3,19 @@ import { WeatherResponse } from "../../shared/typings";
 
 // Define the base URL for the weather API
 // IMPORTANT: This Should be done by .env.production and env.development file, but because of the time constraint, I have hardcoded it here.
-// let WEATHER_API_ENDPOINT;
-// if (import.meta.env.DEV) {
-//     WEATHER_API_ENDPOINT = import.meta.env.VITE_WEATHER_LOCAL_API_URL; //developement mode api
-// } else {
-//     WEATHER_API_ENDPOINT = import.meta.env.VITE_WEATHER_API_URL; //production mode api
-// }
+let WEATHER_API_ENDPOINT;
+if (import.meta.env.DEV) {
+    WEATHER_API_ENDPOINT = import.meta.env.VITE_WEATHER_LOCAL_API_URL; //developement mode api
+} else {
+    WEATHER_API_ENDPOINT = import.meta.env.VITE_WEATHER_API_URL; //production mode api
+}
 
-const WEATHER_API_ENDPOINT = import.meta.env.VITE_WEATHER_API_URL; //production mode api
-
+console.log(import.meta.env.VITE_WEATHER_API_URL)
 
 export const getWeatherApi = async (city: string) => {
   // Construct the API URL with the city parameter
-  const url = `${WEATHER_API_ENDPOINT}/weather/${city}`;
+  const url = `https://weather-api-backend.vercel.app/weather/${city}`;
+  // const url = `${WEATHER_API_ENDPOINT}/weather/${city}`;
   // Fetch the data from the API
   const res = await fetch(url);
   // Check if the response is OK (200-299 status code)
